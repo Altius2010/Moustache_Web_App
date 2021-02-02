f0972f5d75acb6c9ca766508ff6b07528ab41e27
Altius2010 <72147694+Altius2010@users.noreply.github.com>
2021-02-02 18:06:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         nose_x = results[0].pose.nose.x - 25;
-        nose_y = results[0].pose.nose.y + 5;
+        nose_y = results[0].pose.nose.y;
     }
 }
 
