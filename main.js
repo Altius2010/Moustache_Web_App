c7f02f532646f8d0c925a4ea8142d8f62bbb66ef
Altius2010 <72147694+Altius2010@users.noreply.github.com>
2021-02-02 17:30:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+var nose_x = 0;
+var nose_y = 0;
+
+function preload() {
+    moustache = loadImage("https://i.postimg.cc/CxHMw4Kd/Moustache.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Pose Net is loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(moustache, nose_x - 40, nose_y - 10, 75, 50);
+}
+
+function takeSnapshot() {
+    save("my_filter.png");
+}
